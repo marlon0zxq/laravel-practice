3266db1505b3bb234239c47ebfa7f2342cd0dca8
unknown <zxq.fly@gmail.com>
2022-12-09 19:22:51 +0800

    add all

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('view', ['todos'=> Todo::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $todo = new Todo;
+ 
+        $todo->todo = $request->todo;
+ 
+        $todo->save();
+        return redirect()->route('todos');
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id=0)
+    {
+        $todo = Todo::find($id);
+        return view('edit',['todo' => $todo ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id=0)
+    {
+        $todo = Todo::find($id);
+
+        $todo->todo = $request->todo;
+        
+        $todo->save();
+
+        return redirect()->route('todos');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Todo::where('id',$id)->delete();
+        return redirect()->route('todos');
+    }
+}
